15e386473ae7d9ab6e435b8db07c38326bab91be
lilei <lileiabt@gmail.com>
2017-11-20 11:30:45 +0800

    初始化项目

---
+++ LiLCornerRadius/LiLCornerRadius/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiLCornerRadius
+//
+//  Created by wajiu on 2017/9/8.
+//  Copyright © 2017年 wajiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiLCornerRadius/LiLCornerRadius/ViewController.m
@@ -0,0 +1,55 @@
+//
+//  ViewController.m
+//  LiLCornerRadius
+//
+//  Created by wajiu on 2017/9/8.
+//  Copyright © 2017年 wajiu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIImageView+CornerRadius.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithRectCornerType:UIRectCornerAllCorners radius:20.f];
+    imageView.llBackgroundColor = [UIColor redColor];
+    [imageView setFrame:CGRectMake(130, 80, 1, 1)];
+    [self.view addSubview:imageView];
+    
+    
+    UIImageView *imageViewSecond = [[UIImageView alloc] initWithRectCornerType:UIRectCornerBottomLeft | UIRectCornerTopRight radius:20.f];
+    [imageViewSecond setFrame:CGRectMake(130, 280, 150, 150)];
+    imageViewSecond.borderColor = [UIColor greenColor];
+    imageViewSecond.borderWidth = 20;
+    imageViewSecond.llBackgroundColor = [UIColor orangeColor];
+    [self.view addSubview:imageViewSecond];
+    
+    
+    UIImageView *imageViewThird = [[UIImageView alloc] initWithRectCornerType:UIRectCornerBottomRight | UIRectCornerTopLeft radius:20.f];
+    [imageViewThird setFrame:CGRectMake(130, 480, 150, 150)];
+    [self.view addSubview:imageViewThird];
+    
+    
+#pragma mark - setImage anytime
+    imageView.image = [UIImage imageNamed:@"mac_dog"];
+    imageViewSecond.image = [UIImage imageNamed:@"mac_dog"];
+    imageViewThird.image = [UIImage imageNamed:@"mac_dog"];
+    [imageView setFrame:CGRectMake(0, 80, 300, 300)];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
